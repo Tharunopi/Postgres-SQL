fdcbf679b15c15e6715219a37218a0c5cb65828b
Tharun Atithya <tharunaadhi6@gmail.com>
2024-12-20 12:11:46 +0530

    basic statements and functions

--- SQL Statement fundamentals/SELECT statement.sql
+++ SQL Statement fundamentals/SELECT statement.sql
@@ -1,1 +1,10 @@
-SELECT first_name, last_name FROM actor;
+-- Situation
+-- We want to send out a promotional
+-- email to our existing customers!
+
+-- Challenge
+-- Use a SELECT statement to grab the
+-- first and last names of every customer
+-- and their email address.
+
+SELECT first_name, last_name, email FROM customer;
